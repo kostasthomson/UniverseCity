f1a61989b9e5e258ad75c18cf192016b93a5cbfc
xsxsam <54262610+xsxsam@users.noreply.github.com>
2022-03-31 14:48:58 +0300

    Login Page
    Δήμητρα

---
+++ js/home.js
@@ -0,0 +1,18 @@
+
+   
+const greeting = document.querySelector('.greeting');
+
+window.onload = () => {
+    if(!sessionStorage.name){
+        location.href = 'login.html';
+    } else{
+        greeting.innerHTML = `hello ${sessionStorage.name}`;
+    }
+}
+
+const logOut = document.querySelector('.logout');
+
+logOut.onclick = () => {
+    sessionStorage.clear();
+    location.reload();
+}
