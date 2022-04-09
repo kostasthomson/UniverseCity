c579b5cc038288dd776e612c0d371412d767701d
Kostas Thomson <kostasthomson12@gmail.com>
2022-04-09 17:16:48 +0300

    Update DB_retrieve.php

--- backend/THOMSON/DB_retrieve.php
+++ backend/THOMSON/DB_retrieve.php
@@ -1,14 +1,14 @@
 <?php 
     require "./DB_Class.php";
-    $am = htmlspecialchars($_GET["am"]);
-    $pass = htmlspecialchars($_GET["pass"]);
+    $user_am = htmlspecialchars($_GET["am"]);
+    $user_pass = htmlspecialchars($_GET["pass"]);
     
     $table_name = "STUDENT";
     $table_columns = array("am" => "TEXT", "Name" => "TEXT", "Password" => "TEXT");
 
     $db = new DataBase("sqlite:../test.db");
     
-    $query = "SELECT * FROM $table_name WHERE am='$am' AND Password='$pass';";
+    $query = "SELECT * FROM $table_name WHERE am='$user_am' AND Password='$user_pass';";
     $query_done = $db->makeQuery($query);
 
     if($query_done) {
