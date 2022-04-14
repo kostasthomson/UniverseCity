4b0204b930ffd9a18acaf6f2ea93baa98c463d93
xsxsam <54262610+xsxsam@users.noreply.github.com>
2022-04-14 19:52:12 +0300

    BOOTSTRAP

--- js/home.js
+++ js/home.js
@@ -1,14 +1,30 @@
 
    
 const greeting = document.querySelector('.greeting');
+document.getElementById("logo-btn").addEventListener("click", clickOnLogo);
 
 window.onload = () => {
-    if(!sessionStorage.name){
+    if(sessionStorage.name){
         location.href = 'login.html';
     } else{
         greeting.innerHTML = `hello ${sessionStorage.name}`;
     }
 }
+
+function clickOnLogo(){
+    location.href = 'login.html';
+}
+
+function test(){
+    const visible = document.getElementById("Notifications").style.visibility
+    if (visible == "" || visible == "hidden") {
+        document.getElementById("Notifications").style.visibility = "visible"
+        document.getElementById("Notifications").style.transition = "opacity 2s linear";  
+    } else {
+        document.getElementById("Notifications").style.visibility = "hidden";
+    }
+}
+
 
 const logOut = document.querySelector('.logout');
 
