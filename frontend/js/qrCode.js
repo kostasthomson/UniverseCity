18e5e21fe6d821dded52f16d10230222cd2ca4b8
withan46 <72460876+withan46@users.noreply.github.com>
2022-04-30 14:28:45 +0300

    Update qrCode.js

--- frontend/js/qrCode.js
+++ frontend/js/qrCode.js
@@ -1,7 +1,30 @@
+var students = [
+  { studentName: "ics2111", studentGrade: 7, lesson: "MethamaticI" },
+  { studentName: "ics2112", studentGrade: 8, lesson: "MathematicII" },
+  { studentName: "ics2113", studentGrade: 7, lesson: "Statistics" },
+  { studentName: "ics2114", studentGrade: 6, lesson: "MethamaticI" },
+  { studentName: "ics2115", studentGrade: 5, lesson: "MathematicII" },
+  { studentName: "ics2116", studentGrade: 8, lesson: "Statistics" },
+  { studentName: "ics2117", studentGrade: 9, lesson: "MethamaticI" },
+  { studentName: "ics2118", studentGrade: 3, lesson: "Statistics" },
+  { studentName: "ics2119", studentGrade: 6, lesson: "Statistics" },
+  { studentName: "ics2120", studentGrade: 1, lesson: "MathematicII" },
+  { studentName: "ics2121", studentGrade: 3, lesson: "MethamaticI" },
+  { studentName: "ics2122", studentGrade: 7, lesson: "Statistics" },
+]
+
 function onScanSuccess(qrCodeMessage) {
   document.getElementById("result").innerHTML =
     '<span class="result">' + qrCodeMessage + "</span>";
   console.log(document.getElementById("result").innerHTML);
+  var element = document.getElementById("result");
+  //If it isn't "undefined" and it isn't "null", then it exists.
+  console.log(students.includes("ics2121"));
+  if (students.includes("ics2121")) {
+    alert("Element exists");
+  } else {
+    alert("Element does not exist");
+  }
 }
 function onScanError(errorMessage) {
   //handle scan error
