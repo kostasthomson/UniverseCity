f6873746f090288d95dbed3571580d915b3afc77
Savvas Misailidis <savvasmis@hotmail.gr>
2022-05-06 21:09:22 +0300

    αλλαγες στην συσ επιστολ

---
+++ frontend/js/recommendation letter.js
@@ -0,0 +1,20 @@
+function genPDF() {
+	
+    var doc = new jsPDF();          
+    var elementHandler = {
+    '#ignorePDF': function (element, renderer) {
+        return true;
+        }
+    };
+    var source = window.document.getElementsByTagName("body")[0];
+    doc.fromHTML(
+        source,
+        15,
+        15,
+        {
+        'width': 180,'elementHandlers': elementHandler
+        });
+
+    doc.save('test.pdf');
+	
+}
