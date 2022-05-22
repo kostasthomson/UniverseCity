38f584b6b41264c0fa888f3142ea5f553be7331d
kostasthomson <kostasthomson12@gmail.com>
2022-05-22 20:39:24 +0300

    Update login_page.js

--- frontend/js/login_page.js
+++ frontend/js/login_page.js
@@ -27,20 +27,17 @@ form.forEach((item, i) => {
 })
 
 // form validation
-
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const USER_AM = urlParams.get('am');
 const USER_PASS = urlParams.get('pass');
-const BUTTON = document.getElementsByName('button');
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         const dbResult = this.responseText;
-        console.log(dbResult);
-        if(dbResult!="Query failed" && dbResult!="not a registered user") {
+        if(dbResult!="Fail" && dbResult!="Unrecorded") {
             window.location.href="../../NEW/Universecity/index.html";
-            const result_array = dbResult.split(",", 3);
+            const result_array = dbResult.split(",");
             var newUser = new User(result_array[0], result_array[1], result_array[2]);
             if(newUser.am==="ics"){
                 sessionStorage.setItem("user","student");
@@ -49,20 +46,12 @@ xmlhttp.onreadystatechange = function() {
             }else if(newUser.am==="dai"){
                 sessionStorage.setItem("user","secretariat");
             }
-
             newUser.LogData();
         }else {
             console.log(dbResult);
         }
     }
 }; 
-xmlhttp.open("GET","/backend/DB_retrieve.php?am="+USER_AM+"&pass="+USER_PASS,true);
+xmlhttp.open("GET","../../backend/DB_retrieve.php?am="+USER_AM+"&pass="+USER_PASS,true);
 xmlhttp.send();
-
-window.onload=()=>{
-    if(newUser){
-        newUser.deleteData();
-        sessionStorage.clear();
-    }
-};
 
