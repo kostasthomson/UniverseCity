77deb98a09b83ea823fcb6f1fc59bca6a5bbe491
Vasilis Tsavalias <v.tsavalias@gmail.com>
2022-05-24 22:40:37 +0300

    minisculeChanges
    gama8vol5

--- NEW/Universecity/js/evalutationForm.js
+++ NEW/Universecity/js/evalutationForm.js
@@ -1,25 +1,28 @@
 const selectedSub = document.querySelector(".select");
 const  radioBtns = document.querySelectorAll(".styleRadio");
 const text = document.querySelector(".textarea");
+const disabled = document.querySelector(".invalid");
 
 selectedSub.addEventListener('change', (e) => {
     //clear text
     text.value= "";
     let  selectedValue = selectedSub.value;
     
-    
-    for(let i=0;i<radioBtns.length;i++)
+    //clear buttons
+    let size = radioBtns.length;
+    for(let i=0;i<size;i++)
     {
         radioBtns[i].checked = false;
     }
     
-
+    //remove text decoration from disabled button
+    disabled.className= "";
     
    
       
     
     
-    console.log("Selected: " + selectedValue);
-    console.log("gama8");
+    
+   
  });
 
