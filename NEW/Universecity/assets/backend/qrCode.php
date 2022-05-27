15e5abf2351d2fda2dff606bf41b9d35c35cf47f
withan46 <72460876+withan46@users.noreply.github.com>
2022-05-27 01:49:49 -0900

    changes

--- NEW/Universecity/assets/backend/qrCode.php
+++ NEW/Universecity/assets/backend/qrCode.php
@@ -3,7 +3,7 @@
     $user_qrId = htmlspecialchars($_GET["student_pass_id"]); //Take variables from qrCode.js"qrCodeMessage"
 
   
-    $db = new DataBase("sqlite:DATABASES/STORAGE.db");
+    $db = new DataBase("sqlite:DATABASES/STORAGE_fortesting.db");
 
     $query = "SELECT * FROM QRCODE WHERE student_pass_id='$user_qrId' ;";
     $query_done = $db->makeQuery($query);
--- NEW/Universecity/js/qrCode.js
+++ NEW/Universecity/js/qrCode.js
@@ -1,23 +1,29 @@
 function onScanSuccess(qrCodeMessage) {
 
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const dbResult = this.responseText;
-      if(dbResult != "Fail" && dbResult != "No resutls"){
-        console.log("Success");
-      }
-    }
-  };
-  xmlhttp.open("GET","assets/backend/qrCode.php?student_pass_id=" + qrCodeMessage,true);
-  xmlhttp.send();
-  html5QrcodeScanner.render(onScanError);//Stopping on first Camera scan
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            const dbResult = this.responseText;
+            const par = document.createElement("p");
+            par.innerHTML = dbResult;
+            document.getElementById("result").append(par);
+            if (dbResult != "Fail" && dbResult != "No resutls") {
+                console.log("Success");
+
+            }
+        }
+    };
+
+    xmlhttp.open("GET", "assets/backend/qrCode.php?student_pass_id=" + qrCodeMessage, true);
+    xmlhttp.send();
 }
+
 function onScanError(errorMessage) {
-  //handle scan error
+    //handle scan error
 }
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
-  fps: 1,
-  qrbox: 250,
+    fps: 1,
+    qrbox: 250,
 });
 html5QrcodeScanner.render(onScanSuccess, onScanError);
