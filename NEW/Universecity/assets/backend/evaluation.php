acd2f7a968ce09d890fbf9df63279f6c43aefeb7
Savvas Misailidis <savvasmis@hotmail.gr>
2022-05-30 01:56:50 +0300

    evaluation semi done

--- NEW/Universecity/assets/backend/evaluation.php
+++ NEW/Universecity/assets/backend/evaluation.php
@@ -1,23 +1,25 @@
 <?php
-    $subject = htmlspecialchars($_GET["subjects"]);
-    $ans1 = htmlspecialchars($_GET["optradio1"]);
-    $ans2 = htmlspecialchars($_GET["optradio2"]);
-    $ans3 = htmlspecialchars($_GET["optradio3"]);
-    $ans4 = htmlspecialchars($_GET["optradio4"]);
-    $ans5 = htmlspecialchars($_GET["optradio5"]);
-    $ans6 = htmlspecialchars($_GET["optradio6"]);
-    $ans7 = htmlspecialchars($_GET["optradio7"]);
-    $ans8 = htmlspecialchars($_GET["optradio8"]);
-    $ans9 = htmlspecialchars($_GET["optradio9"]);
-    $ans10 = htmlspecialchars($_GET["optradio10"]);
-    $ans11 = htmlspecialchars($_GET["optradio11"]);
-    $ans12 = htmlspecialchars($_GET["optradio12"]);
+    require "./DB_Class.php";
 
+    $queryUrl = $_GET["results"];
+
+    $results = json_decode($queryUrl);
+
+    $keySubject = "subject";
+    $keyOptions = "options";
+    $keyText = "text";
     
-    $table = "EVALUATION";
-    $db = new PDO("sqlite:evaluation_form.db");
-    $db->exec("INSERT INTO ".$table." (Subject, Answer1, Answer2, Answer3, Answer4, Answer5, Answer6, Answer7, Answer8, Answer9, Answer10, Answer11, Answer12) VALUES ('$subject', '$ans1', '$ans2', '$ans3', '$ans4', '$ans5', '$ans6', '$ans7', '$ans8', '$ans9', '$ans10', '$ans11', '$ans12')");
-    
-    
-    
+    $subject = $results->$keySubject;
+    $options = $results->$keyOptions;
+    $text = $results->$keyText;
+
+    $db = new DataBase("sqlite:DATABASES/STORAGE_fortesting.db");
+
+    $query = "INSERT INTO EVALUATION (subject, answer_1, answer_2, answer_3, answer_4, answer_5, answer_6, answer_7, answer_8, answer_9, answer_10, answer_11, answer_12, text) VALUES 
+    ('$subject', ".implode(",", array_values($options))." ,'$text')";
+
+    $db->makeDMLQuery($query);
+
+    $db->close();
+    //echo $subject. "," .implode(",", array_values($options)). "," .$text;
 ?>
