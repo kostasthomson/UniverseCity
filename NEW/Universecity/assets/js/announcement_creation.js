675eea040ff94e2bb664b40cd2dab9c1ba0d7ce7
Vasilis Tsavalias <v.tsavalias@gmail.com>
2022-05-30 17:42:32 +0300

    AnakoinwseisUIandJS

--- NEW/Universecity/assets/js/announcement_creation.js
+++ NEW/Universecity/assets/js/announcement_creation.js
@@ -1,37 +1,37 @@
 function PostAnnouncement(sender) {
-    const title = document.getElementById('announcement-title').value;
-    const description = document.getElementById('announcement-description').value;
+    const title = document.querySelector(".form-control");
+    const description = document.getElementById('floatingTextarea').value;
     const DateTime = new Date();
     let hours = DateTime.getHours();
     let mins = DateTime.getMinutes();
     let seconds = DateTime.getSeconds();
     let year = DateTime.getFullYear();
-    let month = DateTime.getMonth()+1;
+    let month = DateTime.getMonth() + 1;
     let day = DateTime.getDate();
 
-    if(hours < 10)
-        hours = '0'+hours;
-    if(mins < 10)
-        mins = '0'+mins;
-    if(seconds < 10)
-        seconds = '0'+seconds;
-    if(month < 10)
-        month = '0'+month;
-    if(day < 10)
-        day = '0'+day;
+    if (hours < 10)
+        hours = '0' + hours;
+    if (mins < 10)
+        mins = '0' + mins;
+    if (seconds < 10)
+        seconds = '0' + seconds;
+    if (month < 10)
+        month = '0' + month;
+    if (day < 10)
+        day = '0' + day;
     const time = hours + ':' + mins + ':' + seconds;
     const date = year + '-' + month + '-' + day;
-    const queryString = "title="+title+"&description="+description+"&time="+time+"&date="+date+"&sender="+sender;
+    const queryString = "title=" + title + "&description=" + description + "&time=" + time + "&date=" + date + "&sender=" + sender;
     console.log(queryString);
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const dbResult = this.responseText;
-            if(dbResult != 'Done') {
+            if (dbResult != 'Done') {
                 alert(dbResult);
             }
         }
     };
-    xmlhttp.open("GET","../assets/backend/post_announcement.php?"+queryString,true);
+    xmlhttp.open("GET", "../../assets/backend/post_announcement.php?" + queryString, true);
     xmlhttp.send();
 }
