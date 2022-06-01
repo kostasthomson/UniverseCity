e6908b9ceca1c6f9b7fe16e20ee9f10995eb5f37
Savvas Misailidis <savvasmis@hotmail.gr>
2022-06-02 02:57:39 +0300

    fixed evaluation_form

--- NEW/Universecity/assets/backend/subjectGet.php
+++ NEW/Universecity/assets/backend/subjectGet.php
@@ -4,13 +4,11 @@
 
     $db = new DataBase("sqlite:DATABASES/STORAGE_fortesting.db");
 
-    $query = "SELECT code,title FROM SUBJECTS LEFT JOIN STUDENTS ON SUBJECTS.semester = STUDENTS.semester";
+    $query = "SELECT code,title FROM SUBJECTS WHERE SUBJECTS.semester = 4";
     
     $db->makeQuery($query);
 
     $result = $db->getQueryResults();
-
-    
 
     foreach($result as $r){
         echo $r["code"]. "," .$r['title']. ",";
--- NEW/Universecity/js/evalutationForm.js
+++ NEW/Universecity/js/evalutationForm.js
@@ -1,6 +1,6 @@
+// const selectedSub = document.querySelector(".select");
 const selectedSub = document.querySelector(".select");
 const radioBtnsNodes = document.querySelectorAll(".styleRadio");
-const test = document.querySelector(".styleRadio");
 const labelInput = document.querySelector("radio-inline");
 const text = document.querySelector(".textarea");
 const disabled = document.querySelector(".invalid");
@@ -8,29 +8,9 @@ const submitBtn = document.querySelector(".submitBtn");
 const textarea = document.querySelector(".textarea");
 let radioCounter = 0;
 
-
 const RADIO_EACH_ROW = 6;
 
-selectedSub.addEventListener('change', (e) => {
-    //clear text
-    text.value = "";
-    let selectedValue = selectedSub.value;
 
-    //clear buttons
-    let size = radioBtnsNodes.length;
-    for (let i = 0; i < size; i++) {
-        radioBtnsNodes[i].checked = false;
-    }
-
-    //remove text decoration from disabled button
-    disabled.className = "";
-
-    radioCounter = 0;
-
-    submitBtn.style.backgroundColor = "grey";
-    submitBtn.style.boxShadow = "grey"
-
-});
 
 function fillOptions() {
 
@@ -47,12 +27,7 @@ function fillOptions() {
     return array;
 }
 
-
 function sendRequest() {
-
-
-
-
 
     for (radio of radioBtnsNodes) {
         if (radio.checked) {
@@ -60,18 +35,12 @@ function sendRequest() {
         }
     }
 
-
-
     let options = fillOptions();
 
     let subject = document.getElementById("subjs");
     let value = subject.options[subject.selectedIndex].value;
 
     const textArea = document.getElementById("textArea").value;
-
-    // alert(options);
-    // alert(value);
-    // alert(textArea);
 
     let queryObject = {
         "subject": value,
@@ -82,7 +51,6 @@ function sendRequest() {
     };
 
     const jsonQueryObject = JSON.stringify(queryObject);
-    // alert(jsonQueryObject);
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
@@ -99,12 +67,11 @@ function sendRequest() {
                 const result_array = dbResult.split(",");
                 console.log(result_array);
 
+                console.log(selectedSub);
 
             }
         }
     };
-    // xmlhttp.open("POST","assets/backend/evaluation.php");
-    // xmlhttp.send(jsonQueryObject);
 
     xmlhttp.open("GET", "assets/backend/evaluation.php?results=" + jsonQueryObject, true);
     xmlhttp.send();
@@ -114,10 +81,6 @@ window.onload = () => {
     radioCounter = 0;
     submitBtn.style.backgroundColor = "grey";
     submitBtn.style.boxShadow = "grey"
-
-
-
-
 };
 
 
@@ -135,8 +98,33 @@ document.addEventListener("change", (e) => {
     if (counter >= 12) {
         submitBtn.style.backgroundColor = "#366c77";
         submitBtn.style.boxShadow = "0rem 0.7rem 1.2rem #3996a9"
-
-
-
     }
 });
+
+selectedSub.addEventListener('change', (e) => {
+        //clear text
+        text.value = "";
+        let selectedValue = selectedSub.value;
+        console.log("mphka");
+    
+        //clear buttons
+        let size = radioBtnsNodes.length;
+        for (let i = 0; i < size; i++) {
+            radioBtnsNodes[i].checked = false;
+        }
+    
+        //remove text decoration from disabled button
+        disabled.className = "";
+    
+        radioCounter = 0;
+    
+        submitBtn.style.backgroundColor = "grey";
+        submitBtn.style.boxShadow = "grey"
+
+})
+
+
+// .addEventListener('change', (e) => {
+
+
+// });
