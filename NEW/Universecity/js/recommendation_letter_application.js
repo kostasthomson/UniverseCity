0c2823af6c3b0e14a1cb2643e2fa3bec40c2da55
Vasilis Tsavalias <v.tsavalias@gmail.com>
2022-06-02 18:02:21 +0300

    smallchanges
    gama9vol10

--- NEW/Universecity/js/recommendation_letter_application.js
+++ NEW/Universecity/js/recommendation_letter_application.js
@@ -1,3 +1,19 @@
+const inputBtn = document.querySelector(".form-check-input");
+const submitBtn = document.querySelector(".btn.btn-primary");
+const selectSub = document.querySelector(".select");
+
+
+
+window.onload = () => {
+    submitBtn.style.backgroundColor = "grey";
+    submitBtn.style.boxShadow = "grey"
+    submitBtn.disabled = true;
+    inputBtn.disabled = true;
+};
+
+
+
+
 function queryCreator(){
 
     let subject = document.getElementById("subjs");
@@ -16,9 +32,25 @@ function queryCreator(){
             const dbResult = this.responseText;
             if(dbResult!="Query failed") {
                 alert("Η αίτηση έγινε επιτυχώς");
+                window.location.reload(true);
             }
         };
     }
     xmlhttp.open("GET","assets/backend/recommendation_letter_application.php?results=" + jsonQueryObject, true);
     xmlhttp.send();
 }
+
+
+document.addEventListener("change" , (e) => {
+    inputBtn.disabled = false;
+    console.log("%cThis is a blue text", "color:red");
+    console.log("hi");
+    
+});
+
+inputBtn.addEventListener("click" , (e) => {
+    submitBtn.disabled = false;
+    submitBtn.style.backgroundColor = "#366c77";
+    submitBtn.style.boxShadow = "0rem 0.7rem 1.2rem #3996a9"
+
+});
