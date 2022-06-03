088712d68f7d8edde5395970090ee7362dad050d
Savvas Misailidis <savvasmis@hotmail.gr>
2022-06-03 18:20:15 +0300

    some css changes

--- NEW/Universecity/js/subjectGet.js
+++ NEW/Universecity/js/subjectGet.js
@@ -8,6 +8,7 @@ select.setAttribute("class", "select");
 
 let options = document.createElement("option");
 
+
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -30,13 +31,6 @@ xmlhttp.onreadystatechange = function() {
                     k++;
                 }
             } 
-
-            
-
-            
-            options.setAttribute("disabled", "disabled");
-            options.setAttribute("selected", "selected");
-            options.setAttribute("class", "invalid"); 
 
             for(let i=0;i<subject_title.length;i++){
 
