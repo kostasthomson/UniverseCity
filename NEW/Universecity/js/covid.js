4f08bea2fd50274bcd7a233fc651929e5a7c9ca7
Vasilis Tsavalias <v.tsavalias@gmail.com>
2022-06-04 23:38:15 +0300

    test

--- NEW/Universecity/js/covid.js
+++ NEW/Universecity/js/covid.js
@@ -1,132 +1,130 @@
 const inputBtn = document.querySelector(".form-check-input");
 const submitBtn = document.querySelector(".btn.btn-primary");
 const covidPdf = document.querySelector(".form-control");
-let  classroom = Array.from(document.querySelectorAll(".form-control"));
+const inputs = document.querySelectorAll("input[type=text]");
+
+const regexClass = /[^α-ω0-9,-]/gi;
+const regexSeats = /[^0-9,]/g;
+
+let classroom = Array.from(document.querySelectorAll(".form-control"));
 let seats = [];
 
-let flag = false;
+
+
+let temp = classroom.map(obj => obj.value);
+let seatText = seats.map(obj => obj.value);
+
+let classText = temp.map(element => {
+  return element.toUpperCase();
+});
 
 
 
-covidPdf.addEventListener("change" , (e) => {
+
+
+
+// classroom.forEach(function (elem) {
+//   elem.addEventListener("input", (e) => {
+//     let str = "";
+//     let sanitizedString = "";
+//     str = e.target.value;
+//     sanitizedString = str.replace(/[^α-ω0-9,-]/gi, '');
+//     console.log(sanitizedString);
+//     if (sanitizedString !== e.target.value) {
+//       e.target.value = sanitizedString;
+//     }
+//   });
+// });
+
+seats.forEach(function (obj) {
+  obj.addEventListener("input", (el) => {
+    console.log("hi");
+    let str = "";
+    let sanitizedString = "";
+    str = el.target.value;
+    sanitizedString = str.replace(/[0-9,]/g, '');
+    console.log(sanitizedString);
+    if (sanitizedString !== e.target.value) {
+      el.target.value = sanitizedString;
+    }
+  });
+});
+
+
+
+covidPdf.addEventListener("change", (e) => {
   inputBtn.disabled = false;
 });
 
 submitBtn.addEventListener("click", (e) => {
 
-    const classText = classroom.map(obj => obj.value); 
-    const seatText = seats.map( obj => obj.value);
-
-    
-    let validateArray = [];
-    let textArray = [];
-
-    let index = 0;
-    
-    
-
-    for(let i=0;i<classText.length;i++){
-      validateArray[i] = classText[i].split(",");
-
-      console.log(validateArray[i]);
-
-      validateArray[i].forEach(c => {
-        let numArray = [];
-        console.log(c);
-        numArray = c.split(" ");
-                
-
-        if(numArray.includes("")){
-          numArray.shift();
-        }
-   
-        console.log(numArray);
-        if(numArray.includes("ΑΜΦ")){
-          textArray[index] = "ΑΜΦΙΘΕΑΤΡΟ " + numArray[1];
-        }
-        else if((numArray.includes("ΑΙΘ"))){
-          textArray[index] = "ΑΙΘΟΥΣΑ " + numArray[1];
-        }
-        else if(numArray.includes("ΕΡΓ")){
-          textArray[index] = "ΕΡΓΑΣΤΗΡΙΟ " + numArray[1];
-        }
-        else{
-          flag = true;
-        }
-        
-
-        index++;
-      });
-      
-      
-      
-    }
 
 
-    
-    console.log(textArray);
 
-    let queryObject= {
-        "covidList" : classText,
-        "seatList" : seatText
+
+
+
+  let queryObject = {
+    "covidList": classText,
+    "seatList": seatText
+  };
+
+
+
+
+  const jsonQueryObject = JSON.stringify(queryObject);
+
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const dbResult = this.responseText;
+      if (dbResult != "Query failed") {
+        alert("Η αίτηση έγινε επιτυχώς");
+        let resultArray = dbResult.split(",");
+        //console.log(resultArray);
+
+      }
     };
-
-
-    
-
-    const jsonQueryObject = JSON.stringify(queryObject);
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const dbResult = this.responseText;
-            if(dbResult!="Query failed") {
-                alert("Η αίτηση έγινε επιτυχώς");
-                let resultArray = dbResult.split(",");
-                //console.log(resultArray);
-                
-            }
-        };
-    }
-    xmlhttp.open("GET","assets/backend/covid.php?results=" + jsonQueryObject, true);
-    xmlhttp.send();
+  }
+  xmlhttp.open("GET", "assets/backend/covid.php?results=" + jsonQueryObject, true);
+  xmlhttp.send();
 });
 
 
 
 
-inputBtn.addEventListener("click" , (e) => {
-    submitBtn.disabled = false;
-    submitBtn.style.backgroundColor = "#366c77";
-    submitBtn.style.boxShadow = "0rem 0.7rem 1.2rem #3996a9"
+inputBtn.addEventListener("click", (e) => {
+  submitBtn.disabled = false;
+  submitBtn.style.backgroundColor = "#366c77";
+  submitBtn.style.boxShadow = "0rem 0.7rem 1.2rem #3996a9"
 });
 
-function makeDate(){
-    const DATE_DAYS = 5;
-    let count = 0;
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
- 
-    document.getElementById("tab1").innerHTML = day  + "/" + month + "/" + year; 
-   
-    for(let i = 1; i < DATE_DAYS ; i++){
+function makeDate() {
+  const DATE_DAYS = 5;
+  let count = 0;
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
 
-      let text = document.getElementById
+  document.getElementById("tab1").innerHTML = day + "/" + month + "/" + year;
 
-      if( day - count == 0){
-        date.setDate(0) //? set to the last day of previous month
-        year = date.getMonth() + 1;
-        day = date.getDate();
-        month = date.getDate();
-        document.getElementById("tab" +(i + 1)).innerHTML = (day - i) + "/" + month + "/" + year;
-        
-      }
-      else{
-        document.getElementById("tab" + (i + 1)).innerHTML = (day - i) + "/" + month + "/" + year;
-      }
+  for (let i = 1; i < DATE_DAYS; i++) {
+
+    let text = document.getElementById
+
+    if (day - count == 0) {
+      date.setDate(0) //? set to the last day of previous month
+      year = date.getMonth() + 1;
+      day = date.getDate();
+      month = date.getDate();
+      document.getElementById("tab" + (i + 1)).innerHTML = (day - i) + "/" + month + "/" + year;
+
     }
+    else {
+      document.getElementById("tab" + (i + 1)).innerHTML = (day - i) + "/" + month + "/" + year;
+    }
+  }
 }
 
 window.onload = () => {
@@ -134,22 +132,22 @@ window.onload = () => {
   submitBtn.style.boxShadow = "grey"
   submitBtn.disabled = true;
   inputBtn.disabled = true;
-    
+
   classroom.shift();  //?Remove the first element of inputs which is the covid file one- we dont need it 
   classroom.pop();  //?Remove the last element of inputs which is the text one - we dont need that either
   //*! Make two seperate lists, one containing the class and the other one the seats 
 
-  
+
 
   let isSeat = false;
 
   for (let e of classroom) {
-    if(isSeat){
+    if (isSeat) {
       seats.push(e);
-      isSeat = false; 
-    }else{
+      isSeat = false;
+    } else {
       isSeat = true;
-    }  
+    }
 
 
   }
