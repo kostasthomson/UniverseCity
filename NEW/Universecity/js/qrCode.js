713d1452cd3122a20cc83f851eafaf719babb584
withan46 <72460876+withan46@users.noreply.github.com>
2022-06-08 21:47:49 +0300

    Close to finallll!!!!!!!!!!!!!!

--- NEW/Universecity/js/qrCode.js
+++ NEW/Universecity/js/qrCode.js
@@ -2,19 +2,21 @@ function onScanSuccess(qrCodeMessage) {
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      const dbResult = this.responseText;
-      document.getElementById("response").innerHTML = dbResult;
-      // location.replace("QrCodeCorrect.html");
+      // const dbResult = this.responseText;
+      // document.getElementById("response").innerHTML = dbResult;
+      location.replace("QrCodeCorrect.html");
     }
   };
-
+  let am = JSON.parse(sessionStorage.getItem("user")).AM;
   xmlhttp.open(
     "GET",
-    "assets/backend/qrCode.php?classroom_id=" + qrCodeMessage + "&am=" + USER_AM, true);
+    "assets/backend/qrCode.php?classroom_id=" + qrCodeMessage + "&am=" + am, true);
+
   xmlhttp.send();
 }
 
 function onScanError(errorMessage) {
+
   // location.replace("QrCodeFail.html");
 }
 var html5QrcodeScanner = new Html5QrcodeScanner("reader", {
