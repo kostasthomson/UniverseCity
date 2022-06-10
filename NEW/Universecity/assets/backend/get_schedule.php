1d7c7efa27659413347839a0560d04100c0ef437
UOM-dodge <ics21004@uom.edu.gr>
2022-06-11 01:33:56 +0300

    get_schedule
    minor change

--- NEW/Universecity/assets/backend/get_schedule.php
+++ NEW/Universecity/assets/backend/get_schedule.php
@@ -5,6 +5,18 @@
     $department = htmlspecialchars($_GET['department']);
     $semester = htmlspecialchars($_GET['semester']);
 
+    switch ($department) {
+        case "ΕΦΑΡΜΟΣΜΕΝΗΣ ΠΛΗΡΟΦΟΡΙΚΗΣ":
+            $department = "ΕΠ";
+            break;
+        case "ΟΙΚΟΝΟΜΙΚΩΝ ΕΠΙΣΤΗΜΩΝ":
+            $department = "ΟΕ";
+            break;
+        //TODO add more cases
+        default:
+            echo "Incorrect Department!";
+    }
+
     $db = new DataBase("sqlite:DATABASES/STORAGE_fortesting.db");
 
     $query = "SELECT time, Monday, Tuesday, Wednesday, Thursday, Friday FROM SCHEDULE WHERE department='$department' and semester='$semester';";
