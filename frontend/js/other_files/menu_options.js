c7806772056e16a265c209dcc136b63c63e2dcbd
Eleftheria Taggili <taggili.eleftheria@gmail.com>
2022-06-17 13:38:57 +0300

    Directory refactor

--- frontend/js/other_files/menu_options.js
+++
@@ -1,7 +0,0 @@
-
-
-const h1 = document.getElementById('title');
-h1.innerHTML = sessionStorage.getItem('user_option');
-
-const frame = document.getElementById('content');
-frame.contentWindow.location.href = ;
