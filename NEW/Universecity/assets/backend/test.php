b99ab174731e2da9b20f9090ada1ba3ed9e1eb6d
Kostas Thomson <kostasthomson12@gmail.com>
2022-06-17 13:12:36 +0300

    temp commit

--- NEW/Universecity/assets/backend/test.php
+++ NEW/Universecity/assets/backend/test.php
@@ -3,6 +3,35 @@
     require "./DB_Class.php";
     $db = new DataBase("sqlite:DATABASES/STORAGE_fortesting.db");
 
+
+    // $query = "DELETE FROM studentsArrive";
+    // $db->makeQuery($query);
+
+    // $query = "SELECT * FROM enrolled_in";
+
+    // $db->makeQuery($query);
+
+    // $resultId = $db->getQueryResults();
+
+    // $array;
+    // $i=0;
+
+    // foreach($resultId as $r){
+    //     $j=0;
+    //     foreach($r as $c){
+    //         $array[$i][$j] = $c;
+    //         $j++;
+    //     }
+    //     $i++;
+    // }
+
+    // foreach($array as $r){
+    //     $random = rand(5,13);
+    //     $query = "INSERT INTO studentsArrive (students_id, subject_id, sum_arrived, lessons_count) VALUES ('$r[0]', '$r[1]', $random, 13)";
+
+    //     $db->makeQuery($query);
+    // }
+
     // $db->makeQuery("SELECT am, code FROM SUBJECTS JOIN STUDENTS ON SUBJECTS.semester=STUDENTS.semester");
     // $results = $db->getQueryResults();
     // foreach($results as $row) {
