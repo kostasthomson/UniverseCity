9ba5e77852e4578990b9dd6316b009313230ba67
UOM-dodge <ics21004@uom.edu.gr>
2022-06-18 03:01:42 +0300

    bookSeat subjectInfo

--- Universecity/assets/js/bookSeat.js
+++ Universecity/assets/js/bookSeat.js
@@ -121,6 +121,7 @@ function retrieveCourses() {
 
 function makeList() { // New Course List with buttons
     console.log(GLOBAL);
+    document.querySelector('#List2').style.display = "none"; // Make the Subject Info panel hidden
     if (getSchedule()) { 
         let CourseListContainer = document.createElement('div');
         CourseListContainer.setAttribute("class", "course_list");
@@ -154,8 +155,7 @@ function makeList() { // New Course List with buttons
         let message = document.createElement('div');
         message.setAttribute('class', 'message-box');
         message.innerHTML = "Δεν υπάρχει κάποιο μάθημα για να κάνεις κράτηση σήμερα.";
-        document.querySelector('.selectCourse').appendChild(message);
-
+        document.querySelector('#SelectCourse').appendChild(message);
     }
 
     
@@ -164,7 +164,7 @@ function makeList() { // New Course List with buttons
 function getSchedule() {
     GLOBAL.user.schedule = JSON.parse(sessionStorage.getItem("schedule"));
 
-    const date = new Date(); //! zero-based for the month index
+    // const date = new Date(2022,05,17); //! zero-based for the month index
    // console.log(date);
 
     // 2022, 05, 08 --> year, month index, day --> 2022 June 8
@@ -174,7 +174,7 @@ function getSchedule() {
     //     })
     // ); 
 
-    const currentWeekday = date.toLocaleDateString('el', {weekday: 'long'});
+    const currentWeekday = GLOBAL.user.date.toLocaleDateString('el', {weekday: 'long'});
 
 
 
@@ -233,7 +233,7 @@ function retrieveClassroom(courseCode, callback) {
 
 function validate(courseCode) {
     // Find the course and return the classroom
-    generateSubjectInfo(courseCode);
+    getTeacher(courseCode, generateSubjectInfo); // forces getTeacher to be executed FIRST before generateSubjectInfo()
     let selectBtn = document.getElementById("selectBtn");
     selectBtn.style.display = 'block';
     
@@ -266,30 +266,57 @@ function validate(courseCode) {
     }
 }
 
-function generateSubjectInfo(courseCode) {
+function generateSubjectInfo(courseCode, teacherName) {
+    document.querySelector('#List2').style.display = "block"; // Makes the Subject Info panel appear
     let subjectInfo = document.querySelector("#SubjectInfo");
+    subjectInfo.innerHTML = ""; //reset list
 
-    //didaktor
-    //ora
-    //mera
-    //aithousa
-    let teacherName = getTeacher(courseCode); //prepei na ginei async
+    // Current Weekday
+    const currentWeekday = GLOBAL.user.date.toLocaleDateString('el', {weekday: 'long'});
 
+
+    // Teacher Name
+    teacherNameArr = teacherName.split(",")
     let teacher = document.createElement("li");
     teacher.setAttribute("class" , "list-group-item");
-    teacher.innerHTML = teacherName;
-    subjectInfo.appendChild(teacher);    
+    teacher.innerHTML = "<strong>Διδάσκων: </strong>" + teacherNameArr[0] + " " + teacherNameArr[1];
+    subjectInfo.appendChild(teacher);   
     
+    
+    // Day of the week
+    let day = document.createElement("li");
+    day.setAttribute("class", "list-group-item");
+    day.innerHTML = "<strong>Ημέρα: </strong>" + currentWeekday;
+    subjectInfo.appendChild(day); 
+
+
+    // Hour that the subject will take place
+    for (let i=0; i<GLOBAL.user.CourseList.length; i++) {
+        if (GLOBAL.user.CourseList[i].CODE == courseCode) { // find the subject's name using the subject's id/code
+            let hourof = 9 + GLOBAL.user.schedule[currentWeekday].indexOf(GLOBAL.user.CourseList[i].TITLE);
+            let hour = document.createElement("li");
+            hour.setAttribute("class", "list-group-item");
+            hour.innerHTML = "<strong>Ώρα: </strong>" + hourof + ":00";
+            subjectInfo.appendChild(hour); 
+            break;
+        }
+    }
+
+
+    // Classroom of the subject
+    let classroom = document.createElement("li");
+    classroom.setAttribute("class", "list-group-item");
+    classroom.innerHTML = "<strong>Αίθουσα: </strong>" + GLOBAL.Classroom.NAME;
+    subjectInfo.appendChild(classroom);
 }
 
-function getTeacher(courseCode){
+function getTeacher(courseCode, callback){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             const dbResult = this.responseText;
             if (dbResult != "Wrong Subject ID") {
-                // let teacher = JSON.parse(dbResult);
-                return dbResult;
+                callback(courseCode, dbResult);
            
 
 
@@ -642,6 +669,8 @@ function getUserInfo(){ // Gets User Info from Session Storage
     //console.log(GLOBAL.user);
     retriveUserSeat(GLOBAL.user.AM, ()=>{});
     retrieveCourses();
+    const date = new Date(2022,05,17); //2022,05,17
+    GLOBAL.user.date = date;
 }
 
 
